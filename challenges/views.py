63beca9232485d49d6d79f9ae14b29a9c2c65dcc
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-12-15 20:55:15 -0300

    refactor(views): ♻️ extract dict with all months

--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+  "january": "End Django course.",
+  "february": "Fix LinkedIn, WorkaLove and personal web portfolio.",
+  "march": "Maintain a 3,5/week Jiu-Jitsu attendance average.",
+  "april": "Make back-end in Django for Maria's website.",
+  "may": "Learn to work and automate ads and ad analysis.",
+  "june": "Clean my work desk every day.",
+  "july": "Meditate every day.",
+  "august": "Learn some Java framework.",
+  "september": "Make my bed every day.",
+  "october": "Watch a video for each major opening I use in my repertoire.",
+  "november": "Start demo investment portfolio.",
+  "december": "Finish ethical hacking course.",
+
+}
+
 # Create your views here.
 
 
 def monthly_challenge(request, month):
-  if month.lower() == "january":
-    return HttpResponse("End Django course.")
-  elif month.lower() == "february":
-    return HttpResponse("Fix linkedin, Workalove and portfolio.")
-  elif month.lower() == "march":
-    return HttpResponse("Maintain a 3,5/week Jiu Jitsu attendance average.")
+  if month.lower() in monthly_challenges:
+    return HttpResponse(monthly_challenges[month])
   else:
     return HttpResponseNotFound("Month not supported.")
