e2aaaf5723322ff08f765bc87962496a687b5b79
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-12-15 20:27:29 -0300

    feat(urls): ✨ dynamic url for months
    -plus error handling in monthly_challenge view

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-  return HttpResponse("It works.")
+def monthly_challenge(request, month):
+  if month.lower() == "january":
+    return HttpResponse("End Django course.")
+  elif month.lower() == "february":
+    return HttpResponse("Fix linkedin, Workalove and portfolio.")
+  elif month.lower() == "march":
+    return HttpResponse("Maintain a 3,5/week Jiu Jitsu attendance average.")
+  else:
+    return HttpResponseNotFound("Month not supported.")
